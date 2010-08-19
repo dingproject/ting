0240c2fea5be7865240810bd33ef8791f9f38e1d
Mikkel Hoegh <mikkel@hoegh.org>
2010-08-19 22:33:19 +0200

    Fix invalid markup in search pager.

--- modules/ting_search/ting_result_pager.tpl.php
+++ modules/ting_search/ting_result_pager.tpl.php
@@ -1,5 +1,14 @@
+<?php
+// $Id$
+
+/**
+ * @file
+ * Template to render the search pager.
+ */
+?>
 <div id="pager">
 	<div class="navwide"><a class="prev" href="#prev"><?php echo t('Previous') ?></a></div>
 	<div class="nav"><a class="first" href="#first"><?php echo t('First') ?></a></div>
-	<div class="navwide"><a class="next" href="#next"><?php echo t('Next') ?></a></p>
+	<div class="navwide"><a class="next" href="#next"><?php echo t('Next') ?></a></div>
 </div>
+
