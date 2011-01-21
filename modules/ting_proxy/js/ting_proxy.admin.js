f2e9a228f72c0966380a1a553471ef849eb22ad0
Jesper Kristensen <cableman@linuxdev.dk>
2011-01-21 13:59:33 +0100

    Added ting proxy module and change the ting module to use it

---
+++ modules/ting_proxy/js/ting_proxy.admin.js
@@ -0,0 +1,18 @@
+// $Id$
+
+// Attache onClick to remove buttons in the admin UI.
+Drupal.behaviors.tingProxyAdmin = function(context) {
+  $(context).find('.remove').click(function () {
+    var obj = $(this);
+    // Mark as deleted and fix required
+    obj.parent().find('.hidden-deleted').val(1);
+
+    // Slide it up
+    obj.parent().slideToggle('slow', function() {
+      obj.parent().find('.url-text-field').val('deleted');
+    });
+
+    // Cancel normal submit
+    return false;
+  });
+};
