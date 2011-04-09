663f90435d78b560696f788e510d47526c84748c
Mikkel Hoegh <mikkel@hoegh.org>
2011-04-09 18:44:34 +0200

    Interface for custom search ranking.
    [#1731]

---
+++ js/ting_admin_ranking_form.js
@@ -0,0 +1,24 @@
+/**
+ * @file
+ * JavaScript behaviors for ranking field editing form.
+ */
+
+// Add remove-button to each ranking field. 
+Drupal.behaviors.tingAdminRankingForm = function(context) {
+  $(context).find('.ranking-field-instance').each(function () {
+    var field = $(this);
+
+    $('<input class="form-submit" name="remove" type="button" value="' + Drupal.t('Remove') + '" />')
+      .click(function () {
+        // Setting an empty field name will cause the field to be
+        // deleted on the server.
+        field.find('select.field-name').val('');
+
+        // Hide the deleted field.
+        field.parent().slideUp('fast');
+        return false;
+      })
+      .appendTo(field);
+  });
+};
+
